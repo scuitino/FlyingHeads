a22d9637a542611cc5d4efa1931c98f3321e6ec4
scuitino <sebastiancuitino@gmail.com>
2018-11-24 14:35:01 +0100

    Throw zone added

--- Assets/_Game/Scripts/CThrowController.cs
+++ Assets/_Game/Scripts/CThrowController.cs
@@ -13,6 +13,13 @@ public class CThrowController : MonoBehaviour {
     // long press gesture instance
     private LongPressGestureRecognizer _longPressGesture;
 
+    // when the touch needs to start when you want to throw
+    [SerializeField]
+    GameObject _throwZone;
+
+    [SerializeField]
+    LayerMask _throwZoneLayer;
+
     private void Start()
     {
         CreateLongPressGesture();
@@ -25,28 +32,31 @@ public class CThrowController : MonoBehaviour {
     private bool GestureIntersectsSprite(DigitalRubyShared.GestureRecognizer g, GameObject obj)
     {
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(g.StartFocusX, g.StartFocusY, -Camera.main.transform.position.z));
-        Collider2D col = Physics2D.OverlapPoint(worldPos);
+        Collider2D col = Physics2D.OverlapPoint(worldPos, _throwZoneLayer);
         return (col != null && col.gameObject != null && col.gameObject == obj);
     }
 
     // manage long press gesture
     private void LongPressGestureCallback(DigitalRubyShared.GestureRecognizer gesture)
     {
-        if (gesture.State == GestureRecognizerState.Began)
+        if (GestureIntersectsSprite(gesture, _throwZone)) // start throw only if the start position is on the throw zone
         {
-            Debug.Log("empezo");    
-        }
-        else if (gesture.State == GestureRecognizerState.Executing)
-        {
-            // update throw data with touch position
-            Vector2 tWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(gesture.FocusX, gesture.FocusY, -Camera.main.transform.position.z));
-            _launcher.UpdateThrowData(tWorldPos);
-        }
-        else if (gesture.State == GestureRecognizerState.Ended)
-        {
-            Debug.Log("termino");
-            // fire
-            _launcher.launch = true;
+            if (gesture.State == GestureRecognizerState.Began)
+            {
+                Debug.Log("empezo");
+            }
+            else if (gesture.State == GestureRecognizerState.Executing)
+            {
+                // update throw data with touch position
+                Vector2 tWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(gesture.FocusX, gesture.FocusY, -Camera.main.transform.position.z));
+                _launcher.UpdateThrowData(tWorldPos);
+            }
+            else if (gesture.State == GestureRecognizerState.Ended)
+            {
+                Debug.Log("termino");
+                // fire
+                _launcher.launch = true;
+            }
         }
     }
 
